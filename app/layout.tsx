c79a20edd65dca1d51a6576fa8119f92d3b31ddc
boboluya <boboluya@gmail.com>
2026-07-28 17:18:42 +0800

    feat:SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import {
   SITE_DESCRIPTION,
@@ -103,6 +104,7 @@ export default function RootLayout({
         />
         {children}
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
